63e5f73bfde98d63bec2549d56af19ab99dbf529
Jeff Tougas <tougasj@gmail.com>
2010-11-17 23:26:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textcare::Application.config.secret_token = '57dc93fddd5ae41a076d44fe01e851e5b2bd4907b5078c04005694995744a955c9a3d34ae933df94bae2666ae429a7d7b5b5e06c431a06a59a2b1b2b253f5ad8'
